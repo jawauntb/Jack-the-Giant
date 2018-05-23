7e73248313af42df6bf309af1a1b3956bca85042
jawauntb <Juan@JawaunMP.lan1>
2018-05-23 13:24:26 -0400

    scene and class files

---
+++ Game Classes/Scene Classes/GameplayScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameplayScene.swift
+//  Jack The Giant
+//
+//  Created by Jawaun Brown on 5/23/18.
+//  Copyright © 2018 Jawaun Brown. All rights reserved.
+//
+
+import SpriteKit
+
+class GameplayScene: SKScene{
+    
+    override func didMove(to view: SKView) {
+        print("Happy i printed");
+    }
+}
